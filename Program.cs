f356c668d49fea3554bba562805cd5ff2ac7684f
Celiane Simoes <celi_bonifacio@hotmail.com>
2021-09-08 20:54:47 -0300

    criada classe

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio_Carro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
