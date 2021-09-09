7cc322b2cb7d96bfe3eab419181c482028c05160
Celiane Simoes <celi_bonifacio@hotmail.com>
2021-09-08 23:27:32 -0300

    exercicio em resolucao

--- Carro.cs
+++ Carro.cs
@@ -6,5 +6,59 @@ namespace Exercicio_Carro
 {
     class Carro
     {
+
+        public string Nome;
+        public string Cor;
+        public string Modelo;
+        public double Preco;
+
+
+        public Carro(string nome, string cor, string modelo, double preco)
+        {
+            Nome = nome;
+            Cor = cor;
+            Modelo = modelo;
+            Preco = preco;
+        }
+
+        public double Andar()
+        {
+
+        }
+
+        public string RadarNaFrente()
+        {
+            if (RadarNaFrente = sim)
+            {
+                return Console.WriteLine("Radar a frente - reduzindo velocidade")
+                }
+            else
+            {
+                return Console.WriteLine("Tudo Ok! Pisa fundo Bino!!!");
+            }
+
+            public string Parar()
+            {
+
+            }
+            public string DescricaoCompleta(string nome, string cor, string modelo, double preco)
+            {
+                this.DescricaoCompleta = DescricaoCompleta;
+            }
+
+            public string AplicarDesconto()
+            {
+                if (corOpcao == branco || preto)
+                {
+                    return precoComDesconto = preco - (preco * 10);
+                }
+                else
+                {
+                    return DescricaoCompleta;
+                }
+            }
+
+
+        }
     }
 }
