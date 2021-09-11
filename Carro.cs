cb11e15bc1bba0cc102e0d1c208fe58ce216b961
Celiane Simoes <celi_bonifacio@hotmail.com>
2021-09-10 23:24:56 -0300

    Finalizada a resolucao do exercicio carro

--- Carro.cs
+++ Carro.cs
@@ -1,10 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
+using System.Transactions;
+
 
 namespace Exercicio_Carro
 {
     class Carro
     {
+
+        public string Nome;
+        public string Cor;
+        public string Modelo;
+        public double Preco;
+        public double valorComDesconto;
+        public double valorDoDesconto;
+        public string retornoMetodoToString;
+
+
+        public Carro(string nome, string cor, string modelo, double preco)
+        {
+            Nome = nome;
+            Cor = cor;
+            Modelo = modelo;
+            Preco = preco;
+        }
+
+        public Carro()
+        {
+        }
+
+        public void Andar(int kmAPercorrer)
+        {
+            for (int i = 0; i <= kmAPercorrer; i ++)
+            {
+                Console.WriteLine("Percorrido " + ( i + 100) + " Km!");
+            }
+        }
+
+        public void RadarNaFrente(string radar)
+        {
+            if (radar == "sim")
+            {
+                Console.WriteLine("Radar a frente - reduzindo velocidade");
+            }
+            else
+            {
+                Console.WriteLine("Tudo Ok! Pisa fundo Bino!!!");
+            }
+        }
+
+        public void Parar(string testeFinalizado)
+        {
+            if(testeFinalizado == "sim"){
+                Console.WriteLine(" O carro parou!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return " Nome: "
+                + Nome
+                + " , Cor: "
+                + Cor
+                + " , Modelo:"
+                + Modelo
+                + " , Preço:R$ "
+                + this.Preco;
+        }
+
+        public string AplicarDesconto(string cor)
+        {
+            if (cor == "branco" || cor == "preto")
+            {
+                double valorDoDesconto = this.Preco * 0.10;
+                double valorComDesconto = this.Preco - valorDoDesconto;
+                this.Preco = valorComDesconto;
+                return this.ToString();
+            }
+            else
+            {
+                var retornoMetodoToString = ToString();
+                return retornoMetodoToString;
+            }
+        }
+                
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Exercicio_Carro
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Carro carro = new Carro();
+            
+
+            Console.WriteLine("Por gentileza insira os dados do veiculo");
+            Console.Write("Nome: ");
+            carro.Nome = Console.ReadLine();
+
+            Console.Write("Cor: ");
+            carro.Cor = Console.ReadLine();
+
+            Console.Write("Modelo: ");
+            carro.Modelo = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            carro.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("A cada quilometro percorrido imprimir o resultado:");
+            carro.Andar(int.Parse(Console.ReadLine()));
+
+            Console.Write("Informe se há radar a frente:");
+            carro.RadarNaFrente(Console.ReadLine());
+
+            Console.Write("Test Drive finalizado: ");
+            carro.Parar(Console.ReadLine());
+
+            Console.WriteLine ("Descrição para comprador: " + carro.AplicarDesconto(carro.Cor));
+          
+
+
+            
+
+
+
         }
     }
 }
